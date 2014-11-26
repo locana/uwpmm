558265f863ec94ce21d73d1e73bdf6ab8436f7e9
Nao Taco <naotaco@gmail.com>
2014-11-26 22:48:10 +0900

    Port controls for application settings

---
+++ Project/uwpmm/uwpmm.Shared/Control/SettingSection.xaml.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The User Control item template is documented at http://go.microsoft.com/fwlink/?LinkId=234236
+
+namespace Kazyx.Uwpmm.Control
+{
+    public sealed partial class SettingSection : UserControl
+    {
+        string Title
+        {
+            get
+            {
+                return TitleTextBlock.Text;
+            }
+            set
+            {
+                TitleTextBlock.Text = value;
+            }
+        }
+
+        public SettingSection(string SectionTitle)
+        {
+            InitializeComponent();
+            TitleTextBlock.Text = SectionTitle;
+        }
+
+        public void Add(UIElement child)
+        {
+            SettingItems.Children.Add(child);
+        }
+    }
+}
