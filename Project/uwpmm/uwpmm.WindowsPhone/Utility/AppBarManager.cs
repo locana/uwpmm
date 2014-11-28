cedc77fdae7ed0634cf72f130b896fe4ddaad203
kazyx <dev.ykz@gmail.com>
2014-11-29 01:05:01 +0900

    Fix CommandBarManager to create each button for any new request

--- Project/uwpmm/uwpmm.WindowsPhone/Utility/AppBarManager.cs
+++ Project/uwpmm/uwpmm.WindowsPhone/Utility/AppBarManager.cs
@@ -8,37 +8,46 @@ namespace Kazyx.Uwpmm.Utility
 {
     public class CommandBarManager
     {
-
         public CommandBarManager()
         {
-            this.Init();
+            EnabledItems.Add(AppBarItemType.WithIcon, new SortedSet<AppBarItem>());
+            EnabledItems.Add(AppBarItemType.OnlyText, new SortedSet<AppBarItem>());
         }
 
         public CommandBar bar = new CommandBar();
 
-        readonly AppBarButton CameraSettingButton = new AppBarButton() { Icon = new BitmapIcon() { UriSource = new Uri("ms-appx:///Assets/AppBar/appbar_cameraSetting.png", UriKind.Absolute) }, Label = "Camera settings" };
-        readonly AppBarButton AppSettingButton = new AppBarButton() { Icon = new BitmapIcon() { UriSource = new Uri("ms-appx:///Assets/AppBar/feature.settings.png", UriKind.Absolute) }, Label = "Application settings" };
-        readonly AppBarButton AboutPageButton = new AppBarButton() { Label = "About this application" };
-        readonly AppBarButton LoggerPageButton = new AppBarButton() { Label = "Logger" };
-        readonly AppBarButton PlaybackPageButton = new AppBarButton() { Label = "Playback mode" };
-
-        readonly Dictionary<AppBarItem, AppBarButton> AppBarItems = new Dictionary<AppBarItem, AppBarButton>();
-        readonly Dictionary<AppBarItemType, SortedSet<AppBarItem>> EnabledItems = new Dictionary<AppBarItemType, SortedSet<AppBarItem>>();
-
-        void Init()
+        private static AppBarButton NewButton(AppBarItem item)
         {
-            EnabledItems.Add(AppBarItemType.Primary, new SortedSet<AppBarItem>());
-            EnabledItems.Add(AppBarItemType.Secondary, new SortedSet<AppBarItem>());
-            AppBarItems.Add(AppBarItem.ControlPanel, CameraSettingButton);
-            AppBarItems.Add(AppBarItem.AppSetting, AppSettingButton);
-            AppBarItems.Add(AppBarItem.AboutPage, AboutPageButton);
-            AppBarItems.Add(AppBarItem.LoggerPage, LoggerPageButton);
-            AppBarItems.Add(AppBarItem.PlaybackPage, PlaybackPageButton);
+            switch (item)
+            {
+                case AppBarItem.ControlPanel:
+                    return new AppBarButton() { Icon = new BitmapIcon() { UriSource = new Uri("ms-appx:///Assets/AppBar/appbar_cameraSetting.png", UriKind.Absolute) }, Label = "Camera settings" };
+                case AppBarItem.AppSetting:
+                    return new AppBarButton() { Icon = new BitmapIcon() { UriSource = new Uri("ms-appx:///Assets/AppBar/feature.settings.png", UriKind.Absolute) }, Label = "Application settings" };
+                case AppBarItem.AboutPage:
+                    return new AppBarButton() { Label = "About this application" };
+                case AppBarItem.LoggerPage:
+                    return new AppBarButton() { Label = "Logger" };
+                case AppBarItem.PlaybackPage:
+                    return new AppBarButton() { Label = "Playback mode" };
+                default:
+                    throw new NotImplementedException();
+            }
         }
 
-        public CommandBarManager SetEvent(AppBarItem item, Windows.UI.Xaml.RoutedEventHandler handler)
+        private readonly Dictionary<AppBarItemType, SortedSet<AppBarItem>> EnabledItems = new Dictionary<AppBarItemType, SortedSet<AppBarItem>>();
+
+        private readonly Dictionary<AppBarItem, RoutedEventHandler> EventHolder = new Dictionary<AppBarItem, RoutedEventHandler>();
+
+        public CommandBarManager SetEvent(AppBarItem item, RoutedEventHandler handler)
         {
-            AppBarItems[item].Click += handler;
+            EventHolder.Add(item, handler);
+            return this;
+        }
+
+        public CommandBarManager ClearEvents()
+        {
+            EventHolder.Clear();
             return this;
         }
 
@@ -53,12 +62,12 @@ namespace Kazyx.Uwpmm.Utility
 
         public CommandBarManager Icon(AppBarItem item)
         {
-            return Enable(AppBarItemType.Primary, item);
+            return Enable(AppBarItemType.WithIcon, item);
         }
 
         public CommandBarManager NoIcon(AppBarItem item)
         {
-            return Enable(AppBarItemType.Secondary, item);
+            return Enable(AppBarItemType.OnlyText, item);
         }
 
         private CommandBarManager Enable(AppBarItemType type, AppBarItem item)
@@ -86,13 +95,17 @@ namespace Kazyx.Uwpmm.Utility
                 Background = Application.Current.Resources["AppBarBackgroundThemeBrush"] as Brush,
                 Opacity = opacity,
             };
-            foreach (AppBarItem item in EnabledItems[AppBarItemType.Primary])
+            foreach (AppBarItem item in EnabledItems[AppBarItemType.WithIcon])
             {
-                bar.PrimaryCommands.Add(AppBarItems[item]);
+                var button = NewButton(item);
+                button.Click += EventHolder[item];
+                bar.PrimaryCommands.Add(button);
             }
-            foreach (AppBarItem item in EnabledItems[AppBarItemType.Secondary])
+            foreach (AppBarItem item in EnabledItems[AppBarItemType.OnlyText])
             {
-                bar.SecondaryCommands.Add(AppBarItems[item]);
+                var button = NewButton(item);
+                button.Click += EventHolder[item];
+                bar.SecondaryCommands.Add(button);
             }
             return bar;
         }
@@ -105,8 +118,8 @@ namespace Kazyx.Uwpmm.Utility
 
     public enum AppBarItemType
     {
-        Primary,
-        Secondary,
+        WithIcon,
+        OnlyText,
     }
 
     public enum AppBarItem
