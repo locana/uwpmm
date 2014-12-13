8648c3b7c6ae5df8e1bf486a4bf187f93712a8fe
kazyx <dev.ykz@gmail.com>
2014-12-13 15:30:08 +0900

    Refine thumbnail loader

--- Project/uwpmm/uwpmm.Shared/Utility/ThumbnailCacheLoader.cs
+++ Project/uwpmm/uwpmm.Shared/Utility/ThumbnailCacheLoader.cs
@@ -3,19 +3,14 @@ using System;
 using System.IO;
 using System.Net;
 using System.Net.Http;
-using System.Runtime.InteropServices.WindowsRuntime;
 using System.Threading.Tasks;
-using Windows.Graphics.Imaging;
 using Windows.Storage;
-using Windows.UI.Core;
-using Windows.UI.Xaml.Media.Imaging;
 
 namespace Kazyx.Uwpmm.Utility
 {
     public class ThumbnailCacheLoader
     {
         private const string CACHE_ROOT = "thumb_cache/";
-        private const string CACHE_ROOT_TMP = "tmp/thumb_cache/";
 
         private readonly HttpClient HttpClient;
 
@@ -29,7 +24,6 @@ namespace Kazyx.Uwpmm.Utility
         {
             var root = ApplicationData.Current.TemporaryFolder;
             await root.CreateFolderAsync(CACHE_ROOT, CreationCollisionOption.OpenIfExists);
-            await root.CreateFolderAsync(CACHE_ROOT_TMP, CreationCollisionOption.OpenIfExists);
         }
 
         private static readonly ThumbnailCacheLoader instance = new ThumbnailCacheLoader();
@@ -54,9 +48,6 @@ namespace Kazyx.Uwpmm.Utility
 
                 var cacheRoot = await root.GetFolderAsync(CACHE_ROOT);
                 await cacheRoot.DeleteDirectoryRecursiveAsync(false);
-
-                var tmpCacheRoot = await root.GetFolderAsync(CACHE_ROOT_TMP);
-                await tmpCacheRoot.DeleteDirectoryRecursiveAsync(false);
             }
             else
             {
@@ -64,38 +55,28 @@ namespace Kazyx.Uwpmm.Utility
 
                 var uuidRoot = await root.GetFolderAsync(CACHE_ROOT + uuid.Replace(":", "-"));
                 await uuidRoot.DeleteDirectoryRecursiveAsync();
-
-                var uuidTmpRoot = await root.GetFolderAsync(CACHE_ROOT_TMP + uuid.Replace(":", "-"));
-                await uuidTmpRoot.DeleteDirectoryRecursiveAsync();
             }
         }
 
         /// <summary>
-        /// Asynchronously download thumbnail image and return local cache path.
+        /// Asynchronously download thumbnail image and return local storage file.
         /// </summary>
         /// <param name="uuid">UUID of the target device.</param>
         /// <param name="content">Source of thumbnail image.</param>
-        /// <returns>Path to local thumbnail cache.</returns>
-        public async Task<string> GetCachePathAsync(string uuid, ContentInfo content)
+        /// <returns>Local storage file</returns>
+        public async Task<StorageFile> LoadCacheFileAsync(string uuid, ContentInfo content)
         {
             var uri = new Uri(content.ThumbnailUrl);
             var directory = CACHE_ROOT + uuid.Replace(":", "-") + "/";
-            var directory_tmp = CACHE_ROOT_TMP + uuid.Replace(":", "-") + "/";
             var filename = content.CreatedTime.Replace(":", "-").Replace("/", "-") + "--" + Path.GetFileName(uri.LocalPath);
-            var filepath = directory + filename;
-            var filepath_tmp = directory_tmp + filename;
 
-            var folder = ApplicationData.Current.TemporaryFolder;
-            await folder.CreateFolderAsync(directory, CreationCollisionOption.OpenIfExists);
-            await folder.CreateFolderAsync(directory_tmp, CreationCollisionOption.OpenIfExists);
+            var root = ApplicationData.Current.TemporaryFolder;
+            var folder = await root.CreateFolderAsync(directory, CreationCollisionOption.OpenIfExists);
+            var file = await folder.GetFileAsync(filename);
 
-            if (await folder.GetFileAsync(filepath) != null)
+            if (file != null)
             {
-                return filepath;
-            }
-            if (await folder.GetFileAsync(filepath_tmp) != null)
-            {
-                return await GetResizedCachePathAsync(filepath_tmp, filepath);
+                return file;
             }
 
             var res = await HttpClient.GetAsync(uri, HttpCompletionOption.ResponseContentRead);
@@ -105,73 +86,13 @@ namespace Kazyx.Uwpmm.Utility
             }
             using (var stream = await res.Content.ReadAsStreamAsync())
             {
-                if (await folder.GetFileAsync(filepath_tmp) == null)
+                var dst = await folder.CreateFileAsync(filename, CreationCollisionOption.OpenIfExists);
+                using (var outStream = await dst.OpenStreamForWriteAsync())
                 {
-                    var dst = await folder.CreateFileAsync(filepath_tmp);
-                    var outStream = await dst.OpenStreamForWriteAsync();
                     await stream.CopyToAsync(outStream);
                 }
-                return await GetResizedCachePathAsync(filepath_tmp, filepath);
+                return dst;
             }
-        }
-
-        private async Task<string> GetResizedCachePathAsync(string path, string resizedPath)
-        {
-            var tcs = new TaskCompletionSource<string>();
-
-            var dispatcher = SystemUtil.GetCurrentDispatcher();
-            if (dispatcher == null) { throw new InvalidOperationException("Failed to obtain dispatcher"); }
-
-            await dispatcher.RunAsync(CoreDispatcherPriority.Low, async () =>
-            {
-                try
-                {
-                    var root = ApplicationData.Current.TemporaryFolder;
-                    if (await root.GetFileAsync(resizedPath) != null)
-                    {
-                        tcs.TrySetResult(resizedPath);
-                        return;
-                    }
-
-                    var src = await root.CreateFileAsync(path, CreationCollisionOption.OpenIfExists);
-                    var file = await root.CreateFileAsync(resizedPath, CreationCollisionOption.OpenIfExists);
-
-                    using (var srcStream = await src.OpenStreamForReadAsync())
-                    {
-                        var original = new BitmapImage();
-                        original.CreateOptions = BitmapCreateOptions.None;
-                        var rndStream = srcStream.AsRandomAccessStream();
-                        rndStream.Seek(0);
-                        original.SetSource(rndStream);
-                        var max = Math.Max(original.PixelHeight, original.PixelWidth);
-                        var scale = (float)THUMBNAIL_SIZE / (float)max;
-                        var wbmp = new WriteableBitmap((int)(original.PixelWidth * scale), (int)(original.PixelHeight * scale));
-                        rndStream.Seek(0);
-                        wbmp.SetSource(rndStream);
-                        byte[] pixels = null;
-                        using (var pixelStream = wbmp.PixelBuffer.AsStream())
-                        {
-                            pixels = new byte[(uint)pixelStream.Length];
-                            await pixelStream.ReadAsync(pixels, 0, pixels.Length);
-                        }
-                        using (var stream = await file.OpenStreamForWriteAsync())
-                        {
-                            var enc = await BitmapEncoder.CreateAsync(BitmapEncoder.JpegEncoderId, stream.AsRandomAccessStream());
-                            enc.SetPixelData(BitmapPixelFormat.Bgra8, BitmapAlphaMode.Ignore, (uint)wbmp.PixelWidth, (uint)wbmp.PixelHeight, 96, 96, pixels);
-                            await enc.FlushAsync();
-                            DebugUtil.Log("New thumbnail cache: " + resizedPath);
-                            tcs.TrySetResult(resizedPath);
-                        }
-                    }
-                }
-                catch (Exception e)
-                {
-                    DebugUtil.Log(e.StackTrace);
-                    tcs.TrySetException(e);
-                }
-            });
-
-            return await tcs.Task;
         }
     }
 }
