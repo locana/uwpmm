d53fdd9d00ccd4b99fd892d6fa6266b4c7d16e46
Nao Taco <naotaco@gmail.com>
2014-12-13 17:33:59 +0900

    Show shoot/exposure mode icons

--- Project/uwpmm/uwpmm.Shared/DataModel/LiveviewScreenViewData.cs
+++ Project/uwpmm/uwpmm.Shared/DataModel/LiveviewScreenViewData.cs
@@ -23,6 +23,8 @@ namespace Kazyx.Uwpmm.DataModel
                 NotifyChangedOnUI("ShutterButtonEnabled");
                 NotifyChangedOnUI("RecDisplayVisibility");
                 NotifyChangedOnUI("ProgressDisplayVisibility");
+                NotifyChangedOnUI("ShootModeImage");
+                NotifyChangedOnUI("ExposureModeImage");
             };
             Device.Api.AvailiableApisUpdated += (sender, e) =>
             {
@@ -38,6 +40,19 @@ namespace Kazyx.Uwpmm.DataModel
         private static readonly BitmapImage StopImage = new BitmapImage(new Uri("ms-appx:///Assets/LiveviewScreen/Stop.png", UriKind.Absolute));
         private static readonly BitmapImage IntervalStillImage = new BitmapImage(new Uri("ms-appx:///Assets/LiveviewScreen/IntervalStillRecButton.png", UriKind.Absolute));
         private static readonly BitmapImage ContShootingImage = new BitmapImage(new Uri("ms-appx:///Assets/LiveviewScreen/ContShootingButton.png", UriKind.Absolute));
+
+        private static readonly BitmapImage StillModeImage = new BitmapImage(new Uri("ms-appx:///Assets/LiveviewScreen/mode_photo.png", UriKind.Absolute));
+        private static readonly BitmapImage MovieModeImage = new BitmapImage(new Uri("ms-appx:///Assets/LiveviewScreen/mode_movie.png", UriKind.Absolute));
+        private static readonly BitmapImage IntervalModeImage = new BitmapImage(new Uri("ms-appx:///Assets/LiveviewScreen/mode_interval.png", UriKind.Absolute));
+        private static readonly BitmapImage AudioModeImage = new BitmapImage(new Uri("ms-appx:///Assets/LiveviewScreen/mode_audio.png", UriKind.Absolute));
+
+        private static readonly BitmapImage AModeImage = new BitmapImage(new Uri("ms-appx:///Assets/LiveviewScreen/ExposureMode_A.png", UriKind.Absolute));
+        private static readonly BitmapImage IAModeImage = new BitmapImage(new Uri("ms-appx:///Assets/LiveviewScreen/ExposureMode_iA.png", UriKind.Absolute));
+        private static readonly BitmapImage IAPlusModeImage = new BitmapImage(new Uri("ms-appx:///Assets/LiveviewScreen/ExposureMode_iAPlus.png", UriKind.Absolute));
+        private static readonly BitmapImage MModeImage = new BitmapImage(new Uri("ms-appx:///Assets/LiveviewScreen/ExposureMode_M.png", UriKind.Absolute));
+        private static readonly BitmapImage SModeImage = new BitmapImage(new Uri("ms-appx:///Assets/LiveviewScreen/ExposureMode_S.png", UriKind.Absolute));
+        private static readonly BitmapImage PModeImage = new BitmapImage(new Uri("ms-appx:///Assets/LiveviewScreen/ExposureMode_P.png", UriKind.Absolute));
+        private static readonly BitmapImage PShiftModeImage = new BitmapImage(new Uri("ms-appx:///Assets/LiveviewScreen/ExposureMode_P_shift.png", UriKind.Absolute));
 
         public BitmapImage ShutterButtonImage
         {
@@ -60,6 +75,50 @@ namespace Kazyx.Uwpmm.DataModel
                     default:
                         return null;
                 }
+            }
+        }
+
+        public BitmapImage ShootModeImage
+        {
+            get
+            {
+                if (Device.Status.ShootMode == null) { return null; }
+                switch (Device.Status.ShootMode.Current)
+                {
+                    case ShootModeParam.Still:
+                        return StillModeImage;
+                    case ShootModeParam.Movie:
+                        return MovieModeImage;
+                    case ShootModeParam.Interval:
+                        return IntervalModeImage;
+                    case ShootModeParam.Audio:
+                        return AudioImage;
+                }
+                return null;
+            }
+        }
+
+        public BitmapImage ExposureModeImage
+        {
+            get
+            {
+                if (Device.Status.ExposureMode == null) { return null; }
+                switch (Device.Status.ExposureMode.Current)
+                {
+                    case ExposureMode.Intelligent:
+                        return IAModeImage;
+                    case ExposureMode.Superior:
+                        return IAPlusModeImage;
+                    case ExposureMode.Program:
+                        return PModeImage;
+                    case ExposureMode.Aperture:
+                        return AModeImage;
+                    case ExposureMode.SS:
+                        return SModeImage;
+                    case ExposureMode.Manual:
+                        return MModeImage;
+                }
+                return null;
             }
         }
 
@@ -111,7 +170,7 @@ namespace Kazyx.Uwpmm.DataModel
                     case EventParam.ItvRecording:
                         return Visibility.Visible;
                 }
-                
+
                 return Visibility.Collapsed;
             }
         }
