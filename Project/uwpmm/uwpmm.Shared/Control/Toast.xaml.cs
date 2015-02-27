2f26aa5bf97e7b56994e4bb16a8ee666a297d35a
Nao Taco <naotaco@gmail.com>
2015-02-27 23:08:41 +0900

    Refactor codes for animation

--- Project/uwpmm/uwpmm.Shared/Control/Toast.xaml.cs
+++ Project/uwpmm/uwpmm.Shared/Control/Toast.xaml.cs
@@ -33,66 +33,19 @@ namespace Kazyx.Uwpmm.Control
             ToastGrid.DataContext = Contents.ElementAt(0);
             DebugUtil.Log("Dequeue toast:" + Contents.ElementAt(0).Text);
             Running = true;
-            CreateAnimation(ToastGrid.ActualHeight, Orientation.Down, SlideInAnimationCompleted).Begin();
+            AnimationHelper.CreateSlideInAnimation(ToastGrid, AnimationOrientation.Down, TimeSpan.FromMilliseconds(120), SlideInAnimationCompleted).Begin();
         }
 
         async void SlideInAnimationCompleted(object sender, object e)
         {
-            var height = ToastGrid.ActualHeight;
             await System.Threading.Tasks.Task.Delay(TimeSpan.FromMilliseconds(3000));
 
-            CreateAnimation(height, Orientation.Up, (e_, sender_) =>
+            AnimationHelper.CreateSlideInAnimation(ToastGrid, AnimationOrientation.Up, TimeSpan.FromMilliseconds(120), (e_, sender_) =>
             {
                 Contents.RemoveAt(0);
                 Running = false;
                 DequeueToast();
             }).Begin();
-        }
-
-        private Storyboard CreateAnimation(double height, Orientation orientation, EventHandler<object> completed = null)
-        {
-            var sb = new Storyboard();
-            var slide = new DoubleAnimationUsingKeyFrames();
-            var fade = new DoubleAnimationUsingKeyFrames();
-            SlideTransform.Y = -height;
-
-            if (orientation == Orientation.Down)
-            {
-                slide.KeyFrames.Add(new EasingDoubleKeyFrame() { KeyTime = KeyTime.FromTimeSpan(TimeSpan.FromMilliseconds(0)), Value = -height });
-                slide.KeyFrames.Add(new EasingDoubleKeyFrame() { KeyTime = KeyTime.FromTimeSpan(TimeSpan.FromMilliseconds(20)), Value = -height * 0.2 });
-                slide.KeyFrames.Add(new EasingDoubleKeyFrame() { KeyTime = KeyTime.FromTimeSpan(TimeSpan.FromMilliseconds(120)), Value = 0 });
-
-                fade.KeyFrames.Add(new EasingDoubleKeyFrame() { KeyTime = KeyTime.FromTimeSpan(TimeSpan.FromMilliseconds(0)), Value = 0 });
-                fade.KeyFrames.Add(new EasingDoubleKeyFrame() { KeyTime = KeyTime.FromTimeSpan(TimeSpan.FromMilliseconds(20)), Value = 0.8 });
-                fade.KeyFrames.Add(new EasingDoubleKeyFrame() { KeyTime = KeyTime.FromTimeSpan(TimeSpan.FromMilliseconds(120)), Value = 1.0 });
-            }
-            else if (orientation == Orientation.Up)
-            {
-                slide.KeyFrames.Add(new EasingDoubleKeyFrame() { KeyTime = KeyTime.FromTimeSpan(TimeSpan.FromMilliseconds(0)), Value = 0 });
-                slide.KeyFrames.Add(new EasingDoubleKeyFrame() { KeyTime = KeyTime.FromTimeSpan(TimeSpan.FromMilliseconds(20)), Value = -height * 0.5 });
-                slide.KeyFrames.Add(new EasingDoubleKeyFrame() { KeyTime = KeyTime.FromTimeSpan(TimeSpan.FromMilliseconds(120)), Value = -height });
-
-                fade.KeyFrames.Add(new EasingDoubleKeyFrame() { KeyTime = KeyTime.FromTimeSpan(TimeSpan.FromMilliseconds(0)), Value = 1.0 });
-                fade.KeyFrames.Add(new EasingDoubleKeyFrame() { KeyTime = KeyTime.FromTimeSpan(TimeSpan.FromMilliseconds(20)), Value = 0.2 });
-                fade.KeyFrames.Add(new EasingDoubleKeyFrame() { KeyTime = KeyTime.FromTimeSpan(TimeSpan.FromMilliseconds(120)), Value = 0 });
-            }
-
-            Storyboard.SetTargetProperty(slide, "Y");
-            Storyboard.SetTarget(slide, SlideTransform);
-
-            Storyboard.SetTargetProperty(fade, "Opacity");
-            Storyboard.SetTarget(fade, ToastGrid);
-
-            sb.Children.Add(slide);
-            sb.Children.Add(fade);
-            if (completed != null) { sb.Completed += completed; }
-            return sb;
-        }
-
-        enum Orientation
-        {
-            Up,
-            Down,
         }
     }
 
---
+++ Project/uwpmm/uwpmm.Shared/Utility/AnimationHelper.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Media.Animation;
+
+namespace Kazyx.Uwpmm.Utility
+{
+    public class AnimationHelper
+    {
+        public static Storyboard CreateSlideInAnimation(FrameworkElement target, AnimationOrientation orientation, TimeSpan d, EventHandler<object> completed = null)
+        {
+            double distance = 0;
+            var sb = new Storyboard();
+            var slide = new DoubleAnimationUsingKeyFrames();
+            var fade = new DoubleAnimationUsingKeyFrames();
+            var transform = new TranslateTransform();
+            var duration = d.Milliseconds;
+
+            Storyboard.SetTarget(slide, transform);
+            Storyboard.SetTargetProperty(fade, "Opacity");
+            Storyboard.SetTarget(fade, target);
+            target.RenderTransform = transform;
+
+            switch (orientation)
+            {
+                case AnimationOrientation.Up:
+                case AnimationOrientation.Down:
+                    distance = -target.ActualHeight;
+                    transform.Y = distance;
+                    Storyboard.SetTargetProperty(slide, "Y");
+                    break;
+                case AnimationOrientation.Left:
+                case AnimationOrientation.Right:
+                    distance = -target.ActualWidth;
+                    transform.X = distance;
+                    Storyboard.SetTargetProperty(slide, "X");
+                    break;
+            }
+
+            var KeyframeTimes = new List<double>() { 0, duration / 6, duration }; // 3 key frames.
+            List<double> KeyframeDistance = new List<double>();
+            List<double> KeyframeOpacity = new List<double>();
+
+            switch (orientation)
+            {
+                case AnimationOrientation.Down:
+                    KeyframeDistance = new List<double>() { distance, distance * 0.2, 0 };
+                    KeyframeOpacity = new List<double>() { 0, 0.8, 1.0 };
+                    break;
+                case AnimationOrientation.Up:
+                    KeyframeDistance = new List<double>() { 0, distance * 0.5, distance };
+                    KeyframeOpacity = new List<double>() { 1.0, 0.6, 0 };
+                    break;
+                case AnimationOrientation.Left:
+                    // todo: tune-up
+                    KeyframeDistance = new List<double>() { 0, distance * 0.5, distance };
+                    KeyframeOpacity = new List<double>() { 1.0, 0.6, 0 };
+                    break;
+                case AnimationOrientation.Right:
+                    // todo
+                    KeyframeDistance = new List<double>() { distance, distance * 0.2, 0 };
+                    KeyframeOpacity = new List<double>() { 0, 0.8, 1.0 };
+                    break;
+            }
+
+            for (int i = 0; i < KeyframeTimes.Count; i++)
+            {
+                slide.KeyFrames.Add(new EasingDoubleKeyFrame() { KeyTime = KeyTime.FromTimeSpan(TimeSpan.FromMilliseconds(KeyframeTimes[i])), Value = KeyframeDistance[i] });
+                fade.KeyFrames.Add(new EasingDoubleKeyFrame() { KeyTime = KeyTime.FromTimeSpan(TimeSpan.FromMilliseconds(KeyframeTimes[i])), Value = KeyframeOpacity[i] });
+            }
+
+            sb.Children.Add(slide);
+            sb.Children.Add(fade);
+            if (completed != null) { sb.Completed += completed; }
+            return sb;
+        }
+    }
+
+    public enum AnimationOrientation
+    {
+        Up,
+        Down,
+        Left,
+        Right,
+    }
+}
