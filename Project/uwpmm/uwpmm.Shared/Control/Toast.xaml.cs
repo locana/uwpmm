5dd941e8e40dd855fc99c9d1307ed216eb0db0b8
Nao Taco <naotaco@gmail.com>
2015-03-09 23:57:51 +0900

    Support to specify Toast duration

--- Project/uwpmm/uwpmm.Shared/Control/Toast.xaml.cs
+++ Project/uwpmm/uwpmm.Shared/Control/Toast.xaml.cs
@@ -3,7 +3,6 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using Windows.UI.Xaml.Controls;
-using Windows.UI.Xaml.Media.Animation;
 using Windows.UI.Xaml.Media.Imaging;
 
 // The User Control item template is documented at http://go.microsoft.com/fwlink/?LinkId=234236
@@ -19,6 +18,7 @@ namespace Kazyx.Uwpmm.Control
 
         private List<ToastContent> Contents = new List<ToastContent>();
         private bool Running = false;
+        const int SLIDE_DURATION = 120;
 
         public void PushToast(ToastContent content)
         {
@@ -27,28 +27,33 @@ namespace Kazyx.Uwpmm.Control
             if (!Running) { DequeueToast(); }
         }
 
-        void DequeueToast()
+        async void DequeueToast()
         {
             if (Contents.Count == 0) { return; }
-            ToastGrid.DataContext = Contents.ElementAt(0);
-            DebugUtil.Log("Dequeue toast:" + Contents.ElementAt(0).Text);
+
+            var content = Contents.ElementAt(0);
+
+            ToastGrid.DataContext = content;
+            DebugUtil.Log("Dequeue toast:" + content.Text);
             Running = true;
             AnimationHelper.CreateSlideAnimation(new AnimationRequest()
             {
                 Target = ToastGrid,
-                Duration = TimeSpan.FromMilliseconds(120),
-                Completed = SlideInAnimationCompleted
+                Duration = TimeSpan.FromMilliseconds(SLIDE_DURATION),
             }, FadeSide.Top, FadeType.FadeIn).Begin();
-        }
 
-        async void SlideInAnimationCompleted(object sender, object e)
-        {
-            await System.Threading.Tasks.Task.Delay(TimeSpan.FromMilliseconds(3000));
+            var duration = TimeSpan.FromMilliseconds(3000);
+            if (content.Duration != null && content.Duration.Milliseconds > SLIDE_DURATION)
+            {
+                duration = content.Duration;
+            }
+
+            await System.Threading.Tasks.Task.Delay(duration);
 
             AnimationHelper.CreateSlideAnimation(new AnimationRequest()
             {
                 Target = ToastGrid,
-                Duration = TimeSpan.FromMilliseconds(120),
+                Duration = TimeSpan.FromMilliseconds(SLIDE_DURATION),
                 Completed = (e_, sender_) =>
                 {
                     Contents.RemoveAt(0);
@@ -64,5 +69,6 @@ namespace Kazyx.Uwpmm.Control
         public ToastContent() { }
         public String Text { get; set; }
         public BitmapImage Icon { get; set; }
+        public TimeSpan Duration { get; set; }
     }
 }
